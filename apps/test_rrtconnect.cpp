f8fdf7452a48b7ad2cdbdd5dd642ddda772a653e
Dinko Osmankovic <dinko.osmankovic@etf.unsa.ba>
2021-03-16 19:06:24 +0100

    Added flann deps

--- apps/test_rrtconnect.cpp
+++ apps/test_rrtconnect.cpp
@@ -1,9 +1,14 @@
 #include <RRTConnect.h>
 #include <iostream>
 #include <RealVectorSpace.h>
+#include <glog/logging.h>
 
-int main()
+
+int main(int argc, char **argv)
 {
+	google::InitGoogleLogging(argv[0]);
+	FLAGS_logtostderr = true;
+	LOG(INFO) << "GLOG successfully initialized!" << std::endl;
 	//base::RealVectorSpace *space = new base::RealVectorSpace(2);
 	base::StateSpace *ss = new base::RealVectorSpace(2);
 	planning::rrt::RRTConnect* planner = new planning::rrt::RRTConnect(ss);
