9d3f41f553ff252aeed3c90758a8bbe1f97316ab
Dinko Osmankovic <dinko.osmankovic@etf.unsa.ba>
2022-02-07 21:20:34 +0100

    Step 2 of removal

--- src/robots/Planar2DOF.cpp
+++ src/robots/Planar2DOF.cpp
@@ -2,7 +2,11 @@
 // Created by dinko on 07.02.22.
 //
 
+#include <fstream>
+#include <iostream>
+
 #include "Planar2DOF.h"
+#include "urdf/model.h"
 
 robots::Planar2DOF::~Planar2DOF() {}
 
@@ -17,6 +21,15 @@ robots::Planar2DOF::Planar2DOF(std::string robot_desc)
 	{
 		throw std::runtime_error("Failed to construct kdl tree");
 	}
+	std::ifstream t(robot_desc);
+	std::stringstream buffer;
+	buffer << t.rdbuf();
+
+	std::shared_ptr<urdf::UrdfModel> model = urdf::UrdfModel::fromUrdfStr(buffer.str());
+	std::cout << model->getName();
+
+	robot_tree.getChain("base_link", "tool", robot_chain);
+	
 }
 
 const KDL::Tree& robots::Planar2DOF::getRobotTree() const 
