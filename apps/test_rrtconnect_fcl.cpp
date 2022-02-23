6fd3d3dd276c58348335ff827b965a58bd194998
Dinko Osmankovic <dinko.osmankovic@etf.unsa.ba>
2022-02-23 13:15:54 +0100

    Changed test_rrtconnect_fcl to include argument parsing.

--- apps/test_rrtconnect_fcl.cpp
+++ apps/test_rrtconnect_fcl.cpp
@@ -2,23 +2,50 @@
 #include <iostream>
 #include <Scenario.h>
 #include <ConfigurationReader.h>
+#include <CommandLine.h>
 
 #include <glog/logging.h>
-
 
 int main(int argc, char **argv)
 {
 	google::InitGoogleLogging(argv[0]);
-	std::srand((unsigned int) time(0));
+	std::srand((unsigned int)time(0));
 	FLAGS_logtostderr = true;
 	LOG(INFO) << "GLOG successfully initialized!";
 
+	std::string scenarioFilePath = "data/planar_2dof/scenario_easy.yaml";
+	bool printHelp = false;
+
+	CommandLine args("Test RRTConnect command line parser.");
+	args.addArgument({"-s", "--scenario"}, &scenarioFilePath, "Scenario .yaml description file path");
+	args.addArgument({"-h", "--help"},     &printHelp,
+      "Use --scenario scenario_yaml_file_path to "
+      "run with different scenario");
+
+	try
+	{
+		args.parse(argc, argv);
+	}
+	catch (std::runtime_error const &e)
+	{
+		std::cout << e.what() << std::endl;
+		return -1;
+	}
+
+	// When oPrintHelp was set to true, we print a help message and exit.
+	if (printHelp)
+	{
+		args.printHelp();
+		return 0;
+	}
+
 	ConfigurationReader::initConfiguration();
-	
-	scenario::Scenario scenario("data/planar_2dof/scenario_easy.yaml");
-	
+
+	scenario::Scenario scenario(scenarioFilePath);
+
 	std::shared_ptr<base::StateSpace> ss = scenario.getStateSpace();
 
+	LOG(INFO) << "Using scenario: " << scenarioFilePath;
 	LOG(INFO) << "Dimensions: " << ss->getDimensions();
 	LOG(INFO) << "StateSpace Type: " << ss->getStateSpaceType();
 	try
@@ -27,7 +54,7 @@ int main(int argc, char **argv)
 		bool res = planner->solve();
 		LOG(INFO) << "RRTConnect planning finished with " << (res ? "SUCCESS!" : "FAILURE!");
 		LOG(INFO) << "number of nodes: " << planner->getPlannerInfo()->getNumNodes();
-			
+
 		if (res)
 		{
 			std::vector<std::shared_ptr<base::State>> path = planner->getPath();
@@ -36,12 +63,10 @@ int main(int argc, char **argv)
 				std::cout << path.at(i) << std::endl;
 			}*/
 			// TODO: read from configuration yaml
-			
 		}
 		planner->outputPlannerData("/tmp/plannerData.log");
-
 	}
-	catch (std::domain_error& e)
+	catch (std::domain_error &e)
 	{
 		LOG(ERROR) << e.what();
 	}
