89263d02ac4f5e4c4e9f77a8f8caba6e2b4e674e
Dinko Osmankovic <dinko.osmankovic@etf.unsa.ba>
2022-05-17 21:42:13 +0200

    More argument parsing for visualizer.

--- visualizer/run_visualizer_xarm6.py
+++ visualizer/run_visualizer_xarm6.py
@@ -5,18 +5,14 @@ import argparse
 
 
 if __name__ == "__main__":
-    parser = LogParser("/tmp/plannerData.log")
-    path = parser.get_path()
-    
     arg_parser = argparse.ArgumentParser(description='Processing visualizer input argumetns')
     arg_parser.add_argument('-s', '--scenario', default='../data/xarm6/scenario_easy.yaml', help='Scenario file path')
-    
+    arg_parser.add_argument('-l', '--log_path', default='/tmp/plannerData.log', help='Log file path')
     args = arg_parser.parse_args()
-    print(args.scenario)
-    #for p in path:
-    #    print(p)
-    #print(path)
-    #visualize(path[0], "test.png")
+    
+    parser = LogParser(args.log_path)
+    path = parser.get_path()    
+
     with open(args.scenario, 'r') as file:
         obstacles = yaml.safe_load(file)
     visualize(path, obstacles=obstacles, image_file=None, is_trajectory=True, fps=10.0)
