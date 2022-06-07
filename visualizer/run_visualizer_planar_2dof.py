3737277c044e5fc3a44767ea06062e8758d01a58
Dinko Osmankovic <dinko.osmankovic@etf.unsa.ba>
2022-06-07 11:33:19 +0200

    comminting some changes

--- visualizer/run_visualizer_planar_2dof.py
+++ visualizer/run_visualizer_planar_2dof.py
@@ -2,18 +2,21 @@ from log_parser import LogParser
 from planar_2dof_vis import visualize
 import yaml
 from math import pi
+import argparse
 
 
 if __name__ == "__main__":
-    parser = LogParser("/tmp/plannerData.log")
-    path = parser.get_path()
-    #for p in path:
-    #    print(p)
-    #print(path)
-    #visualize(path[0], "test.png")
-    with open('../data/planar_2dof/scenario2.yaml', 'r') as file:
+    arg_parser = argparse.ArgumentParser(description='Processing visualizer input argumetns')
+    arg_parser.add_argument('-s', '--scenario', default='../data/xarm6/scenario_easy.yaml', help='Scenario file path')
+    arg_parser.add_argument('-l', '--log_path', default='/tmp/plannerData.log', help='Log file path')
+    args = arg_parser.parse_args()
+    
+    #parser = LogParser(args.log_path)
+    #path = parser.get_path()   
+    
+    with open(args.scenario, 'r') as file:
         obstacles = yaml.safe_load(file)
-    visualize(path, obstacles=obstacles, image_file=None, is_trajectory=True, fps=10.0) 
-    # path = [-2, -2.5]
-    # visualize(path, obstacles=obstacles, image_file=None, is_trajectory=False, fps=10.0)
+    #visualize(path, obstacles=obstacles, image_file=None, is_trajectory=True, fps=10.0) 
+    path = [-2, -2.5]
+    visualize(path, obstacles=obstacles, image_file=None, is_trajectory=False, fps=10.0)
     
